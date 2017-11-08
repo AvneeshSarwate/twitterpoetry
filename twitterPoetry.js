24b143afcba9ecf0417afc0981e70e83510fd398
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-11-08 02:01:16 -0500

    getting custom text drawn on a path with d3

--- twitterPoetry.js
+++ twitterPoetry.js
@@ -77,6 +77,30 @@ codeBirdSearch("a crisis of faith", null, function(tweets){queryBible(tweets, 46
 var bibleMatch = [];
 var matchResponse;
 
+var svg;
+var svgText;
+$(function() {
+//Create the SVG
+svg = d3.select("body").append("svg")
+        .attr("width", 400)
+        .attr("height", 120);
+            
+//Create an SVG path            
+svg.append("path")
+    .attr("id", "wavy") //very important to give the path element a unique ID to reference later
+    .attr("d", "M 10,90 Q 100,15 200,70 Q 340,140 400,30") //Notation for an SVG path, from bl.ocks.org/mbostock/2565344
+    .style("fill", "none")
+    .style("stroke", "#AAAAAA");
+
+//Create an SVG text element and append a textPath element
+svgText = svg.append("text")
+   .append("textPath") //append a textPath to the text element
+    .attr("xlink:href", "#wavy") //place the ID of the path here
+    .style("text-anchor","middle") //place the text halfway on the arc
+    .attr("startOffset", "50%");
+
+svgText.text("Yay, my text is on a wavy path");
+});
 
 function setup() {
     createCanvas(1280, 720);
@@ -91,6 +115,7 @@ function draw() {
         text(bibleMatch[tweetIndex][0], 0, 200);
         text(bibleMatch[tweetIndex][1], 0, 30);
         tweetIndex = (tweetIndex + 1) % bibleMatch.length;
+        svgText.text(bibleMatch[tweetIndex][1]);
     }
 }
 
